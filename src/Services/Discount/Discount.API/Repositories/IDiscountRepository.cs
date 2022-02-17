2729dbabdf47a4dc8ea9df70a1f5d600367fa89b
Saad Qais <saadqais2013@live.com>
2022-02-17 13:38:53 +0300

    working with discount api

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.API.Entities;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetByProductNameAsync(string productName);
+        Task<bool> CreateAsync(Coupon coupon);
+        Task<bool> UpdateAsync(Coupon coupon);
+        Task<bool> DeleteAsync(string productName);
+    }
+}
