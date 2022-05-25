c402dfbe20e05c19352127c78099d4598d823633
Saad Qais <saadqais2013@live.com>
2022-05-25 14:13:35 +0300

    complete checkout command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(order => order.UserName)
+                .NotEmpty().WithMessage("{UserName} cannot be empty.")
+                .NotNull().WithMessage("{UserName} cannot be null.")
+                .MaximumLength(50).WithMessage("{UserName} cannot be longer than 50 characters.");
+
+            RuleFor(order => order.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} cannot be empty.");
+
+            RuleFor(order => order.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} cannot be empty.")
+                .GreaterThan(0).WithMessage("{TotalPrice} cannot be less than 0.");
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList.ViewModels;
 using Ordering.Domain.Entities;
 
@@ -9,6 +10,7 @@ namespace Ordering.Application.Mappings
         public MappingProfile()
         {
             CreateMap<Order, OrderVM>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
         }
     }
 }
