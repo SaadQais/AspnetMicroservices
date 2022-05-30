ee20dfb4444aa6d94d31da7a3718e2f082a82a26
Saad Qais <saadqais2013@live.com>
2022-05-30 14:18:13 +0300

    complete Persistence

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderDbContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderDbContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() 
+                {
+                    UserName = "SaadQais", 
+                    FirstName = "Saad", 
+                    LastName = "Qais", 
+                    EmailAddress = "saadqais2013@live.com", 
+                    AddressLine = "Bahcelievler", 
+                    Country = "Iraq", 
+                    TotalPrice = 350 
+                }
+            };
+        }
+    }
+}
