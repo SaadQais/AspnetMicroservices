e5a61ec56bdc6f63849d4177790740081e36ae4e
Saad Qais <saadqais2013@live.com>
2022-07-15 18:47:34 +0300

    publish web app project

---
+++ src/WebApps/BasicWebApplication/Services/BasketService.cs
@@ -0,0 +1,41 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+
+namespace AspnetRunBasics.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/Baskets/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+
+        public async Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            var response = await _client.PostAsJson($"/Baskets", model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<BasketModel>();
+            else
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+
+        public async Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            var response = await _client.PostAsJson($"/Baskets/Checkout", model);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+    }
+}
