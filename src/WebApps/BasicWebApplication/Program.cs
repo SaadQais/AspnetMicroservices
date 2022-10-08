796f13b0974abd11666dab641fc06e78f93e7f45
Saad Qais <saadqais2013@live.com>
2022-10-08 15:22:59 +0300

    add serilog packages

--- src/WebApps/BasicWebApplication/Program.cs
+++ src/WebApps/BasicWebApplication/Program.cs
@@ -1,8 +1,28 @@
 using AspnetRunBasics.Services;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
+
+builder.Host.UseSerilog((ctx, confg) =>
+{
+    confg
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(ctx.Configuration["ElasticConfiguration:Uri"]))
+        {
+            IndexFormat = $"applogs-{ctx.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{ctx.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfShards = 2,
+            NumberOfReplicas = 1
+        })
+        .Enrich.WithProperty("Environment", ctx.HostingEnvironment.EnvironmentName)
+        .Enrich.WithProperty("Application", ctx.HostingEnvironment.ApplicationName)
+        .ReadFrom.Configuration(ctx.Configuration); ;
+
+});
 
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
                 c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
